de2ec6fb49b085c9edec16bcb1236adef69cc4d8
vania <i.p.yeremenko@gmail.com>
2021-11-20 21:35:01 +0200

    Labs 1-4

--- Exercises/4-count-types.js
+++ Exercises/4-count-types.js
@@ -1,5 +1,11 @@
 'use strict';
-
-const countTypesInArray = null;
-
+const countTypesInArray = (lol) => {
+  const counter = {};
+  for (const element of lol) {
+    const type = typeof element;
+    const count = counter[type] || 0;
+    counter[type] = count + 1;
+  }
+  return counter;
+};
 module.exports = { countTypesInArray };
